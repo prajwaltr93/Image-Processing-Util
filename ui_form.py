3fa214f43b303f5dfa67543ffdd958cafc601089
Prajwal T R <prajwaltr93@gmail.com>
2024-05-04 10:54:01 +0530

    add: organized the layout a bit

--- ui_form.py
+++ ui_form.py
@@ -24,9 +24,17 @@ class Ui_MainWindow(object):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(1280, 720)
-        self.widget_2 = QWidget(MainWindow)
+        self.videoFrame = QLabel(MainWindow)
+        self.videoFrame.setObjectName(u"videoFrame")
+        self.videoFrame.setGeometry(QRect(10, 0, 1011, 711))
+        self.layoutWidget = QWidget(MainWindow)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(1030, 0, 256, 711))
+        self.verticalLayout_3 = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.widget_2 = QWidget(self.layoutWidget)
         self.widget_2.setObjectName(u"widget_2")
-        self.widget_2.setGeometry(QRect(1020, 0, 221, 251))
         self.verticalLayout_2 = QVBoxLayout(self.widget_2)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.convertGrayScale = QRadioButton(self.widget_2)
@@ -50,11 +58,13 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.label = QLabel(self.widget_2)
         self.label.setObjectName(u"label")
+        self.label.setMaximumSize(QSize(16777215, 20))
 
         self.horizontalLayout.addWidget(self.label)
 
         self.label_2 = QLabel(self.widget_2)
         self.label_2.setObjectName(u"label_2")
+        self.label_2.setMaximumSize(QSize(16777215, 20))
 
         self.horizontalLayout.addWidget(self.label_2)
 
@@ -63,6 +73,7 @@ class Ui_MainWindow(object):
 
         self.horizontalSlider = QSlider(self.widget_2)
         self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setMaximumSize(QSize(16777215, 20))
         self.horizontalSlider.setOrientation(Qt.Horizontal)
 
         self.verticalLayout.addWidget(self.horizontalSlider)
@@ -77,74 +88,83 @@ class Ui_MainWindow(object):
 
         self.pointSelect1 = QHBoxLayout()
         self.pointSelect1.setObjectName(u"pointSelect1")
-        self.spinBox = QSpinBox(self.widget_2)
-        self.spinBox.setObjectName(u"spinBox")
+        self.pointSelect1X = QSpinBox(self.widget_2)
+        self.pointSelect1X.setObjectName(u"pointSelect1X")
 
-        self.pointSelect1.addWidget(self.spinBox)
+        self.pointSelect1.addWidget(self.pointSelect1X)
 
         self.label_3 = QLabel(self.widget_2)
         self.label_3.setObjectName(u"label_3")
 
         self.pointSelect1.addWidget(self.label_3)
 
-        self.spinBox_2 = QSpinBox(self.widget_2)
-        self.spinBox_2.setObjectName(u"spinBox_2")
+        self.pointSelect1Y = QSpinBox(self.widget_2)
+        self.pointSelect1Y.setObjectName(u"pointSelect1Y")
 
-        self.pointSelect1.addWidget(self.spinBox_2)
+        self.pointSelect1.addWidget(self.pointSelect1Y)
 
 
         self.verticalLayout_2.addLayout(self.pointSelect1)
 
         self.pointSelect2 = QHBoxLayout()
         self.pointSelect2.setObjectName(u"pointSelect2")
-        self.spinBox_3 = QSpinBox(self.widget_2)
-        self.spinBox_3.setObjectName(u"spinBox_3")
+        self.pointSelect2X = QSpinBox(self.widget_2)
+        self.pointSelect2X.setObjectName(u"pointSelect2X")
 
-        self.pointSelect2.addWidget(self.spinBox_3)
+        self.pointSelect2.addWidget(self.pointSelect2X)
 
         self.label_4 = QLabel(self.widget_2)
         self.label_4.setObjectName(u"label_4")
 
         self.pointSelect2.addWidget(self.label_4)
 
-        self.spinBox_4 = QSpinBox(self.widget_2)
-        self.spinBox_4.setObjectName(u"spinBox_4")
+        self.pointSelect2Y = QSpinBox(self.widget_2)
+        self.pointSelect2Y.setObjectName(u"pointSelect2Y")
 
-        self.pointSelect2.addWidget(self.spinBox_4)
+        self.pointSelect2.addWidget(self.pointSelect2Y)
 
 
         self.verticalLayout_2.addLayout(self.pointSelect2)
 
-        self.videoFrame = QLabel(MainWindow)
-        self.videoFrame.setObjectName(u"videoFrame")
-        self.videoFrame.setGeometry(QRect(10, 0, 1000, 650))
-        self.layoutWidget = QWidget(MainWindow)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(1020, 280, 254, 26))
-        self.horizontalLayout_2 = QHBoxLayout(self.layoutWidget)
+
+        self.verticalLayout_3.addWidget(self.widget_2)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setSpacing(0)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.loadVideo = QPushButton(self.layoutWidget)
         self.loadVideo.setObjectName(u"loadVideo")
+        self.loadVideo.setMaximumSize(QSize(16777215, 20))
 
         self.horizontalLayout_2.addWidget(self.loadVideo)
 
         self.exportVideo = QPushButton(self.layoutWidget)
         self.exportVideo.setObjectName(u"exportVideo")
+        self.exportVideo.setMaximumSize(QSize(16777215, 20))
 
         self.horizontalLayout_2.addWidget(self.exportVideo)
 
         self.playPause = QPushButton(self.layoutWidget)
         self.playPause.setObjectName(u"playPause")
+        self.playPause.setMaximumSize(QSize(16777215, 20))
 
         self.horizontalLayout_2.addWidget(self.playPause)
 
-        self.previewWindow = QLabel(MainWindow)
-        self.previewWindow.setObjectName(u"previewWindow")
-        self.previewWindow.setGeometry(QRect(1020, 390, 251, 181))
-        self.previewWindowText = QLabel(MainWindow)
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+
+        self.previewWindowText = QLabel(self.layoutWidget)
         self.previewWindowText.setObjectName(u"previewWindowText")
-        self.previewWindowText.setGeometry(QRect(1020, 360, 121, 16))
+        self.previewWindowText.setMaximumSize(QSize(16777215, 50))
+
+        self.verticalLayout_3.addWidget(self.previewWindowText)
+
+        self.previewWindow = QLabel(self.layoutWidget)
+        self.previewWindow.setObjectName(u"previewWindow")
+        self.previewWindow.setMaximumSize(QSize(16777215, 400))
+
+        self.verticalLayout_3.addWidget(self.previewWindow)
+
 
         self.retranslateUi(MainWindow)
 
@@ -153,6 +173,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.videoFrame.setText("")
         self.convertGrayScale.setText(QCoreApplication.translate("MainWindow", u"Convert to Gray Scale", None))
         self.skeletonize.setText(QCoreApplication.translate("MainWindow", u"Skeletonize", None))
         self.showContours.setText(QCoreApplication.translate("MainWindow", u"Show Contours", None))
@@ -161,11 +182,10 @@ class Ui_MainWindow(object):
         self.cropButton.setText(QCoreApplication.translate("MainWindow", u"Crop", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Point 1 (x1, y1)", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"Point 2 (x1, y1)", None))
-        self.videoFrame.setText("")
         self.loadVideo.setText(QCoreApplication.translate("MainWindow", u"Load Video", None))
         self.exportVideo.setText(QCoreApplication.translate("MainWindow", u"Export Data", None))
         self.playPause.setText(QCoreApplication.translate("MainWindow", u"Play", None))
-        self.previewWindow.setText("")
         self.previewWindowText.setText(QCoreApplication.translate("MainWindow", u"Preview Window", None))
+        self.previewWindow.setText("")
     # retranslateUi
 

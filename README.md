95e40fc7fe5dbc99276cdeaeba403fb238f74e4e
Prajwal T R <prajwaltr93@gmail.com>
2024-05-10 17:50:03 +0530

    add: README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Image Processing Util
+![Interface](./resources/image.png)
---
+++ rc_resources.py
@@ -0,0 +1,115 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.7.0
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x04`\
+<\
+!DOCTYPE svg PUB\
+LIC \x22-//W3C//DTD\
+ SVG 1.1//EN\x22 \x22h\
+ttp://www.w3.org\
+/Graphics/SVG/1.\
+1/DTD/svg11.dtd\x22\
+>\x0d\x0a<!-- Uploaded\
+ to: SVG Repo, w\
+ww.svgrepo.com, \
+Transformed by: \
+SVG Repo Mixer T\
+ools -->\x0a<svg fi\
+ll=\x22#000000\x22 wid\
+th=\x22800px\x22 heigh\
+t=\x22800px\x22 viewBo\
+x=\x220 0 32 32\x22 id\
+=\x22icon\x22 xmlns=\x22h\
+ttp://www.w3.org\
+/2000/svg\x22>\x0a\x0d<g \
+id=\x22SVGRepo_bgCa\
+rrier\x22 stroke-wi\
+dth=\x220\x22/>\x0a\x0d<g id\
+=\x22SVGRepo_tracer\
+Carrier\x22 stroke-\
+linecap=\x22round\x22 \
+stroke-linejoin=\
+\x22round\x22/>\x0a\x0d<g id\
+=\x22SVGRepo_iconCa\
+rrier\x22> <defs> <\
+style> .cls-1 { \
+fill: none; } </\
+style> </defs> <\
+title>image--sea\
+rch</title> <pat\
+h d=\x22M24,14a5.99\
+,5.99,0,0,0-4.88\
+5,9.4712L14,28.5\
+859,15.4141,30l5\
+.1147-5.1147A5.9\
+971,5.9971,0,1,0\
+,24,14Zm0,10a4,4\
+,0,1,1,4-4A4.004\
+5,4.0045,0,0,1,2\
+4,24Z\x22/> <path d\
+=\x22M17,12a3,3,0,1\
+,0-3-3A3.0033,3.\
+0033,0,0,0,17,12\
+Zm0-4a1,1,0,1,1-\
+1,1A1.0009,1.000\
+9,0,0,1,17,8Z\x22/>\
+ <path d=\x22M12,24\
+H4V17.9966L9,13l\
+5.5859,5.5859L16\
+,17.168l-5.5859-\
+5.5855a2,2,0,0,0\
+-2.8282,0L4,15.1\
+68V4H24v6h2V4a2.\
+0023,2.0023,0,0,\
+0-2-2H4A2.002,2.\
+002,0,0,0,2,4V24\
+a2.0023,2.0023,0\
+,0,0,2,2h8Z\x22/> <\
+rect id=\x22_Transp\
+arent_Rectangle_\
+\x22 data-name=\x22&lt\
+;Transparent Rec\
+tangle&gt;\x22 clas\
+s=\x22cls-1\x22 width=\
+\x2232\x22 height=\x2232\x22\
+/> </g>\x0a\x0d</svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x09\
+\x0alxC\
+\x00r\
+\x00e\x00s\x00o\x00u\x00r\x00c\x00e\x00s\
+\x00\x0c\
+\x07o='\
+\x00a\
+\x00p\x00p\x00-\x00i\x00c\x00o\x00n\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00(\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x8fb%[#\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
